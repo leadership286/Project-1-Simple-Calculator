451fc0056f770af08a4d5030f779a412e1b6a5e9
leadership286 <159952102+leadership286@users.noreply.github.com>
2024-03-20 15:14:19 +0500

    Add files via upload

---
+++ Simple_Calculator.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+//Asking Question from User through inquirer
+let answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    { message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division'],
+    }
+]);
+//Conditional Statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
+console.log(answer);
